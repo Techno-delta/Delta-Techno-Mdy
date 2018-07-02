30bb4bc0f56022b679a4663d4334a78a3c0a97bf
thetkolin <thetkolin2981834@gmail.com>
2018-07-02 20:54:10 +0630

    This code for home.php

---
+++ nav_bar.php
@@ -0,0 +1,9 @@
+
+<div id="navbar_menu">
+	<ul class="home">
+		<li><a href="#">Company Detail</a></li>
+		<li><a href="#">Search Company</a></li>
+		<li><a href="#">Register / Sign In</a></li>
+		<li><a href="#">Contact Me</a></li>
+	</ul>
+</div>
